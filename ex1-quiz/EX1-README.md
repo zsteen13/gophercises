f2e396925097abd87de8643261c329c325a607e3
Zachary Steen <zsteen13@gmail.com>
2023-12-27 01:00:46 -0600

    ex1: finished part 1, added readme

---
+++ ex1-quiz/EX1-README.md
@@ -0,0 +1,5 @@
+# EX1 - Quiz
+See https://github.com/gophercises/quiz/tree/master.
+
+## Running
+`go build quiz.go && ./quiz`
---
+++ ex1-quiz/quiz.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+)
+
+type QAPair struct {
+	Question, Answer string
+}
+
+func main() {
+	// Open the CSV file
+	file, err := os.Open("problems.csv")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	// Create a new reader
+	reader := csv.NewReader(file)
+
+	// Read the CSV data
+	p := 0
+	c := 0
+	for {
+		record, err := reader.Read()
+		if err != nil {
+			// Handle end-of-file error
+			if err.Error() == "EOF" {
+				break
+			}
+			fmt.Println("Error reading CSV:", err)
+			return
+		}
+
+		// Handle invalid records
+		if len(record) < 2 {
+			fmt.Println("Invalid record:", record)
+			continue
+		}
+
+		// Put record into QAPair object, increment problem number
+		qa := QAPair{
+			Question:	record[0],
+			Answer: 	record[1],
+		}
+		p += 1
+
+		// Print question and await user answer
+		fmt.Printf("Problem #%d: %s = ", p, qa.Question)
+		var input string
+		fmt.Scanln(&input)
+
+		// Increment correct count if user input matches answer
+		if input == qa.Answer {
+			c += 1
+		}
+	}
+
+	fmt.Printf("You scored %d out of %d.\n", c, p)
+}
